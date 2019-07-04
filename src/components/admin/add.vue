21923fbf8f0b1b164905a780ce1284b8488fd09e
FurugenYu <furugen098@gmail.com>
2019-07-04 22:51:47 +0900

    チェックリスト作成画面のモックを作成

--- src/components/admin/add.vue
+++ src/components/admin/add.vue
@@ -1,5 +1,48 @@
 <template>
-    <div >
-        追加
-    </div>
+  <div id="login-container">
+    <div id="login-form">
+      <v-form ref="form" lazy-validation>
+          <div class="invite-container">
+            <v-text-field  label="e-mail" required class="left-area"></v-text-field>
+            <v-text-field  label="password" required class="right-area"></v-text-field>
+          </div>
+        <v-btn to="/admin/list">ログイン</v-btn>
+      </v-form>
+    </div>  
+  </div>
 </template>
+
+<style lang="scss">
+#login-container {
+  padding: 20px;
+  color: darkgray;
+}
+#login-form {
+  padding: 40px;
+  background-color: white;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.invite-container {
+    display: grid;
+    grid-template-columns: 45%  10% 45%;
+    grid-template-areas: 
+        "left space right";
+}
+.left-area {
+    grid-area: left;
+}
+.right-area {
+    grid-area: right;
+}
+</style>
+
+
+<script>
+// @ is an alias to /src
+
+export default {
+  components: {},
+};
+</script>
--- src/components/admin/checklist.vue
+++ src/components/admin/checklist.vue
@@ -1,5 +1,41 @@
 <template>
-    <div >
-        チェックリスト
+  <div style="padding:20px;">
+    <h1>戸締りチェックリストを作成しましょう。</h1>
+    <v-card style="margin : 10px;">
+      <v-card-title>
+        No.1
+        <v-text-field label="戸締り内容を入力してください。" style="width: 100%;"></v-text-field>
+        <v-switch label="写真アップロードをする"></v-switch>
+      </v-card-title>
+    </v-card>
+
+    <v-btn color="primary">チェック追加</v-btn>
+
+    <div style="margin-top:30px;"></div>
+
+    <h1>戸締りをする日時を設定しよう。</h1>
+
+    <div style="margin : 20px">
+      <h2>時間を設定</h2>
+      <div style="padding-left : 20px; padding-top: 10px;">
+        <v-time-picker format="24hr" landscape color="blue lighten-1"></v-time-picker>
+      </div>
     </div>
+
+    <div style="margin : 20px">
+      <h2>日付を設定</h2>
+
+      <div style="padding-left : 20px;">
+        <v-radio-group>
+          <v-radio label="毎日" value="radio-1"></v-radio>
+          <v-radio label="月曜日から金曜日" value="radio-2"></v-radio>
+          <v-radio label="曜日を選択" value="radio-3"></v-radio>
+          <v-radio label="特定の日付" value="radio-4"></v-radio>
+        </v-radio-group>
+      </div>
+    </div>
+
+    <br />
+    <v-btn color="primary">チェックリストを作成する。</v-btn>
+  </div>
 </template>
