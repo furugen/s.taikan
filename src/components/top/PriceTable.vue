4901d0327d493728b360de9b3ee53c95f14d6500
FurugenYu <furugen098@gmail.com>
2019-07-06 09:50:04 +0900

    料金プランのベーススタイルをコミット

--- src/components/top/PriceTable.vue
+++ src/components/top/PriceTable.vue
@@ -1,121 +1,70 @@
 <template>
   <div class="plan-container">
-    <div class="plan-content" style="grid-column: 1;">
-      <div class="plan-header">ビジネスプラン</div>
-      <div class="plan-body">
-        <div>500円 / 月</div>
-        <div>
-          <ul>
-            <li>aaa</li>
-            <li>aaa</li>
-            <li>aaa</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div style="grid-area:desc-head"></div>
+    <div style="grid-area:d1">戸締りチェックリストの作成可能数</div>
+    <div style="grid-area:d2">メンバー数</div>
+    <div style="grid-area:d3">通知機能</div>
+    <div style="grid-area:d4">容量制限</div>
+    
 
-    <div class="plan-content" style="grid-column: 2;">
-      <div class="plan-header">ビジネスプラン</div>
-      <div class="plan-body">
-        <div>500円 / 月</div>
-        <div>
-          <ul>
-            <li>aaa</li>
-            <li>aaa</li>
-            <li>aaa</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div style="grid-area:free-head" class="plan-header-free">無料プラン</div>
+    <div style="grid-area:f1">3</div>
+    <div style="grid-area:f2">3</div>
 
-    <div class="plan-content" style="grid-column: 3;">
-      <div class="plan-header">ビジネスプラン</div>
-      <div class="plan-body">
-        <div>500円 / 月</div>
-        <div>
-          <p style="font-size:large;">参加メンバー ５人まで</p>
-          <p style="font-size:large;">写真レポートの保存期間 1ヶ月</p>」
-        </div>
-      </div>
-    </div>
+
+    <div style="grid-area:family-head" class="plan-header-family">家族向けプラン</div>
+    <div style="grid-area:m1">3</div>
+    <div style="grid-area:m2">10</div>
+
+
+    <div style="grid-area:business-head" class="plan-header-business">ビジネスプラン</div>
+    <div style="grid-area:b1">無制限</div>
+    <div style="grid-area:b2">無制限</div>
+
   </div>
 </template>
 
 <style lang="scss">
 
+.plan-header {
+  padding: 10px;
+  text-align: center;
+}
+
+.plan-header-free {
+  @extend .plan-header;
+  background-color: gray;
+  color: white;
+}
+
+
+.plan-header-family {
+  @extend .plan-header;
+  background-color: greenyellow;
+
+}
+
+
+
+.plan-header-business {
+  @extend .plan-header;
+  background-color: orange;
+  color: white;
+}
+
 
 .plan-container {
   display: grid;
-
-  grid-template-columns: 30% 30% 30%;
+  width: 80%;
+  grid-template-columns: 40% 20% 20% 20%;
   grid-template-areas: 
-    "sp1 left sp2 center sp3"
+    "desc-head free-head family-head business-head"
+    "d1 f1 m1 b1"
+    "d2 f2 m2 b2" 
+    "d3 f3 m3 b3" 
+    "d4 f4 m4 b4" 
+    "d5 f5 m5 b5" 
 }
 
-.plan-content {
-  border: darkgray;
-  border-style: solid;
-  border-width: 1px;
-}
-.plan-header {
-  background-color: gray;
-  color: white;
-  padding: 18px;
-}
-.plan-body {
-  padding: 10px;
-}
 
-.service-plan-wrapper {
-  margin-bottom: 4em;
-  display: flex;
-  flex-wrap: wrap;
-}
-.service-plan-wrapper .inner {
-  width: calc(40% - 10px);
-  margin: 5px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.14);
-  background: #fff;
-  border-radius: 5px;
-}
-.service-plan-wrapper .head {
-  padding: 2em 0.5em;
-  color: #fff;
-  text-align: center;
-}
-.service-plan-wrapper .head .title {
-  font-size: 20px;
-}
-.service-plan-wrapper .head .year {
-  margin-top: 1em;
-}
-.service-plan-wrapper .head .price {
-  font-size: 30px;
-  font-weight: bold;
-}
-.service-plan-wrapper .txt {
-  padding: 0.5em;
-  height: 6em;
-  font-size: 14px;
-}
-.service-plan-wrapper .function-list {
-  padding: 0.5em;
-  font-size: 14px;
-}
-.service-plan-wrapper .function-list ul {
-  margin-left: 0;
-  padding-left: 0;
-  list-style: none;
-}
-.service-plan-wrapper .function-list ul li {
-  padding: 0.5em 0 0.5em 25px;
-  border-top: 1px solid #e5e5e5;
-  background-image: url(); /*（）にアップロードした画像URLを記述する */
-  background-repeat: no-repeat;
-  background-position: 0% 15%;
-}
-.service-plan-wrapper .none {
-  color: #ddd;
-  background-image: none;
-}
 </style>
